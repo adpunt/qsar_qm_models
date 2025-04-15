8c7abae8cdfb23d887a881a43cd0732833d9592e
adpunt <apunt@hmc.edu>
2025-04-15 22:29:57 +0100

    fixup! Setup script with modifications to make rust build platform-independent

--- rust/build.rs
+++ rust/build.rs
@@ -3,8 +3,18 @@ use std::path::PathBuf;
 
 #[cfg(target_os = "macos")]
 fn link_macos_flags() {
+    println!("cargo:rustc-link-lib=c++");          // libc++
+    println!("cargo:rustc-link-lib=iconv");
+    println!("cargo:rustc-link-lib=System");
     println!("cargo:rustc-link-arg=-mmacosx-version-min=13.0");
     println!("cargo:rustc-link-arg=-stdlib=libc++");
+}
+
+#[cfg(target_os = "linux")]
+fn link_linux_flags() {
+    println!("cargo:rustc-link-lib=stdc++");       // GNU libstdc++
+    // iconv may be part of glibc, but add if needed
+    println!("cargo:rustc-link-lib=iconv");
 }
 
 fn main() {
@@ -30,17 +40,14 @@ fn main() {
         println!("cargo:rustc-link-lib={}", lib);
     }
 
-    // Standard + system libs
-    println!("cargo:rustc-link-lib=c++");
-    println!("cargo:rustc-link-lib=boost_serialization");
-    println!("cargo:rustc-link-lib=iconv");
-    println!("cargo:rustc-link-lib=System");
-    println!("cargo:rustc-link-lib=c");
-    println!("cargo:rustc-link-lib=m");
-
     #[cfg(target_os = "macos")]
     link_macos_flags();
 
-    // Embed the RPATH so dyld can find RDKit at runtime
+    #[cfg(target_os = "linux")]
+    link_linux_flags();
+
+    println!("cargo:rustc-link-lib=c");  // always safe
+    println!("cargo:rustc-link-lib=m");
+
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", micromamba_lib.display());
 }
